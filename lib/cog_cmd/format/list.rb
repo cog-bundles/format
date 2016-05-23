9b225cf86b71941b72e704c24061684767d7ce0d
Mark Imbriaco <mark@imbriaco.com>
2016-05-23 17:03:49 -0400

    bump gem and add a simple list command that demonstrates named options

---
+++ lib/cog_cmd/format/list.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+require 'cog/command'
+require 'text-table'
+
+class CogCmd::Format::List < Cog::Command
+  input :accumulate
+
+  def run_command
+    return unless step == :last
+
+    list = fetch_input.map { |obj| obj[request.args[0]] }.sort
+    list = list.reverse if request.options['order'] == 'desc'
+
+    response.template = 'preformatted'
+    response['body'] = list.join(', ')
+  end
+end
