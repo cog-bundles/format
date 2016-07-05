eb713c19c45cd8cecbde176a1a16a549f247eafd
Christopher Maier <chris@operable.io>
2016-07-05 16:28:35 -0400

    list command refinements
    * returns nothing if no input contains the given field
    * More strict about sort order specification
    
    Includes unit tests

--- lib/cog_cmd/format/list.rb
+++ lib/cog_cmd/format/list.rb
@@ -1,19 +1,38 @@
 #!/usr/bin/env ruby
 
 require 'cog/command'
-require 'text-table'
 
 class CogCmd::Format::List < Cog::Command
   input :accumulate
 
   def run_command
     return unless step == :last
+    field = request.args[0]
+    order = get_sort_order
 
-    list = fetch_input.map { |obj| obj[request.args[0]] }.sort
-    list = list.reverse if request.options['order'] == 'desc'
-    delim = request.options['join'] || ', '
+    list = fetch_input.map { |obj| obj[field] }.compact.sort
 
-    response.template = 'preformatted'
-    response['body'] = list.join(delim)
+    if !list.empty?
+      list = list.reverse if order == 'desc'
+      delim = request.options['join'] || ', '
+
+      response.template = 'preformatted'
+      response['body'] = list.join(delim)
+    end
   end
+
+  private
+
+  def get_sort_order
+    order = request.options["order"]
+    case order
+    when nil
+      "asc"
+    when "asc", "desc"
+      order
+    else
+      fail("Unrecognized sort order: use \"asc\" or \"desc\"")
+    end
+  end
+
 end
